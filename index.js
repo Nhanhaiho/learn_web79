fa3152c1c82f2a2feef0fdc1412518445d33e72a
Hai Nhan <hainhan233@gmail.com>
2024-05-31 14:06:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import http from 'http';
+
+const app = http.createServer((req, res) => {
+    res.end('hello world!');
+})
+
+const PORT = 3001
+
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log('server is disconnect')
+    }
+    console.log(`server starting at http://localhost:${PORT}`)
+})
